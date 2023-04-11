803df16d698be5afd5bb8767237c8975be69b089
BILLAUD Ryan <rn.billaud@gmail.com>
2023-04-11 16:03:06 +0200

    Partie 1 fini

--- tlm.hs
+++ tlm.hs
@@ -1,4 +1,6 @@
 import Data.Char
+import Data.List (unfoldr)
+import Data.Time.Clock.POSIX (getPOSIXTime)
 
 tousLesMots :: String -> [String]
 tousLesMots ch = let low = map toLower ch in
@@ -8,15 +10,22 @@ tousLesMots ch = let low = map toLower ch in
 genererTable :: [String] -> [(String,[String])]
 genererTable [] = []
 genererTable [x] = [(x,[])]
-genererTable (x:y:xs)=
+genererTable (x:y:xs) =
         case lookup x tuples of
                 Nothing -> (x,[y]):tuples
                 Just ys -> (x,ys ++ [y]): filter ((/= x). fst) tuples
         where tuples = genererTable(y:xs)
 
+-- Fin partie 1
+
+genererNewText :: [String] -> [String]
+genererNewText [] = []
+genererNewText (x:xs) = []
+
 
 -- "Je suis en train de decouvrir le langage de programmation Haskell, et je suis tres content. Je pense que Haskell est le meilleur langage de programmation au monde." in
 main :: IO()
 main = do
+
         source <- getContents
         print (genererTable (tousLesMots source))
